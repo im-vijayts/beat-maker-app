dd71cc131c778c6d0137962f5eece523decb9920
im-vijayts <vijayts2001@gmail.com>
2020-01-02 19:22:51 +0530

    Project completed

--- MusicApp.java
+++ MusicApp.java
@@ -11,13 +11,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import java.util.ArrayList;
-import java.io.ByteArrayInputStream;
+
 // For playing the audio
 import java.io.File;
 import java.io.IOException;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem; 
 import javax.sound.sampled.Clip; 
@@ -39,10 +37,6 @@ class MusicApp implements ActionListener {
     JFrame root = new JFrame("Music Producer"); 
     
     ArrayList<JButton> btns = new ArrayList<JButton>();
-    final JButton record = new JButton("Record");
-    final JButton stop  = new JButton("Stop");
-    
-    Boolean recording = false;
 
     MusicApp() {
         root.setSize(dimensionx, dimensiony);
@@ -74,23 +68,7 @@ class MusicApp implements ActionListener {
             g -= 2;
         }
 
-        JPanel record_section = new JPanel(new GridLayout(1, 2));
-
-        record.setBackground(Color.WHITE);
-        record.setForeground(Color.BLACK);
-        record.setFocusPainted(false);
-        record.addActionListener(this);
-
-        stop.setBackground(Color.WHITE);
-        stop.setForeground(Color.RED);
-        stop.setFocusPainted(false);
-        stop.addActionListener(this);
-
-        record_section.add(record);
-        record_section.add(stop);
-
         wrapper.add(buttons_section);
-        wrapper.add(record_section);
 
         root.add(pane);
 
@@ -104,33 +82,14 @@ class MusicApp implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e){
-
-        if (e.getSource() == record) {
-            if (!recording){
-                recording = true;
-                System.out.println("Record");
-                record.setEnabled(false);
-                stop.setEnabled(true);
-            }
-        } 
-        else if (e.getSource() == stop){
-            if (recording){
-                recording = false;
-                System.out.println("Stop");
-                stop.setEnabled(false);
-                record.setEnabled(true);
-            }
-        }
-        else{
-            for(JButton button: btns){
-                if (e.getSource() == button){
-                    try {
-                        String sound_path = "./sounds/" + button.getText().toLowerCase() + ".wav";
-                        AudioPlayer player = new AudioPlayer(sound_path, recording);
-                        player.start();
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
+        for(JButton button: btns){
+            if (e.getSource() == button){
+                try {
+                    String sound_path = "./sounds/" + button.getText().toLowerCase() + ".wav";
+                    AudioPlayer player = new AudioPlayer(sound_path);
+                    player.start();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
             }
         }
@@ -150,19 +109,13 @@ class AudioPlayer extends Thread {
 
     Boolean status;
 
-    Boolean recording;
-    
-    // writing to files
-    File fileOut = new File("./output/x.wav");
-
     // constructor to initialize streams and clip
-    public AudioPlayer(String f_path, Boolean recording_status) throws UnsupportedAudioFileException, IOException, LineUnavailableException { 
+    public AudioPlayer(String f_path) throws UnsupportedAudioFileException, IOException, LineUnavailableException { 
         file_path = f_path;
-        recording = recording_status;
         
         // create AudioInputStream object
         audioInputStream = AudioSystem.getAudioInputStream(new File(file_path).getAbsoluteFile()); 
-        
+
         // create clip reference
         clip = AudioSystem.getClip();
         
@@ -175,9 +128,6 @@ class AudioPlayer extends Thread {
         //start the clip 
         clip.start();
         status = true;
-        if (recording) {
-            AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, fileOut);
-        }
     }
 
     // Method to stop the audio
@@ -193,8 +143,8 @@ class AudioPlayer extends Thread {
             while(status){
                 // sleep for 5 seconds in order for the audio to be completely played
                 // because the audio will play only as long as the thread is alive and 
-                // the thread will be alive only as long asw the obljects are created and destroyed
-                // which is veyr much less than 5 seconds
+                // the thread will be alive only as long as the obljects are created and destroyed
+                // which is very much less than 5 seconds
                 Thread.sleep(5_000);
                 status = false;
             }
